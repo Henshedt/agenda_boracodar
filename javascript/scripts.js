316235fb4973fbaf93260c8da3fb7c8a6ec21be8
guilherme fiuza <hensh.edt@gmail.com>
2023-04-27 14:53:48 -0300

    initial commit

---
+++ javascript/scripts.js
@@ -0,0 +1,27 @@
+const filterElement = document.querySelector('header input')
+const contacts = document.querySelectorAll('section ul')
+
+
+
+filterElement.addEventListener('input', filterContacts)
+
+function filterContacts() {
+    if (filterElement.value != '') {
+        for (let contact of contacts) {
+            let title = contact.querySelector('h3')
+            title = title.textContent.toLowerCase()
+            let filterText = filterElement.value.toLowerCase()
+            if (!title.includes(filterText)) {
+                contact.style.display = "none"
+            }
+            else {
+                contact.style.display = "block"
+            }
+        }
+    }
+    else {
+        for (let contact of contacts) {
+            contact.style.display = "block"
+        }
+    }
+}
